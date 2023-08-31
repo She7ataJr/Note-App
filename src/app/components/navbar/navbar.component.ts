fae6e165f90b9c136cb1172d25b2398fee204bba
She7ataJr <She7ataJr@gmail.com>
2023-08-31 13:51:47 +0300

    sign out fixed

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component ,ChangeDetectorRef} from '@angular/core';
+import { Component } from '@angular/core';
 import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth.service';
 @Component({
@@ -7,30 +7,31 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  NoteToken:any
-  loggedIn:boolean=true
-  menuName:string="Login"
-  constructor(private _Router:Router ,public _AuthService:AuthService,private cdRef: ChangeDetectorRef){
+  NoteToken: any
+  menuName: string = "Login"
+  islogin!: boolean
+  constructor(private _Router: Router, public _AuthService: AuthService) {
+    this._AuthService.user.subscribe((res) => {
+      if (res) {
+        this.islogin = true
+      } else {
+        this.islogin = false
+      }
+    })
     this._Router.events.subscribe({
-      next:res=>{
-        
-        if(res instanceof NavigationEnd ){
-          this.menuName = res.url.replace("/","")
+      next: res => {
+
+        if (res instanceof NavigationEnd) {
+          this.menuName = res.url.replace("/", "")
         }
-        
+
       }
     })
   }
- async signOut(){
+  signOut() {
     this._AuthService.user.next(null)
-   console.log('before change the flag')
-    this.loggedIn=false
-   console.log('after change th flag');
-   console.log(this.loggedIn);
-   
-   localStorage.removeItem('NoteToken')
-   await this._Router.navigate(['/login'])
-    this.cdRef.detectChanges();
+    localStorage.removeItem('NoteToken')
+    this._Router.navigate(['/login'])
   }
 
 }
